10cb792f60dda6318913dfaa3bb028a16d0a1e83
Khelil Sator <khelil.sator.git@gmail.com>
2018-05-15 18:15:23 +0200

    Update request_tracker.py

--- request_tracker.py
+++ request_tracker.py
@@ -1,10 +1,20 @@
 import rt
+import salt.runner
 
-uri = 'http://172.30.52.150:9081/REST/1.0/'
-username = 'root'
-password = 'password'
+def get_rt_pillars(): 
+    opts = salt.config.master_config('/etc/salt/master')
+    runner = salt.runner.RunnerClient(opts)
+    pillar = runner.cmd('pillar.show_pillar')
+    uri = pillar['rt']['uri']
+    username = pillar['rt']['username']
+    password = pillar['rt']['password']
+    return {'uri': uri, 'username': username, 'password': password}
 
 def create_ticket(subject, text):
+    rt_pillars=get_rt_pillars()
+    uri = rt_pillars['uri']
+    username = rt_pillars['username']
+    password = rt_pillars['password']
     tracker = rt.Rt(uri, username, password)
     tracker.login()
     ticket_id = tracker.create_ticket(Queue='General', Subject=subject, Text=text)
@@ -12,10 +22,14 @@ def create_ticket(subject, text):
     return ticket_id
 
 def update_ticket(ticket_id, text):
+    rt_pillars=get_rt_pillars()
+    uri = rt_pillars['uri']
+    username = rt_pillars['username']
+    password = rt_pillars['password']
     tracker = rt.Rt(uri, username, password)
     tracker.login()
     tracker.reply(ticket_id, text=text)
     tracker.logout()
 
-ticket_id = create_ticket('subject_example', 'text_example')  
-update_ticket(ticket_id, 'text_update')
+# ticket_id = create_ticket('subject_example', 'text_example')  
+# update_ticket(ticket_id, 'text_update')
